d84feb4d3fd7c4bfd86a4226d4b67d576587fe09
Dami Kang <dami@SJHs-MacBook.local>
2026-01-31 12:18:10 +0900

    Implement vercel Analytics

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,7 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/next"
 import App from './App'
 
 const state = window.__INITIAL_STATE__ || {};
@@ -11,6 +12,7 @@ console.log(initialPosts, "client array check");
 hydrateRoot(
   document.getElementById('root')!,
   <StrictMode>
+    <Analytics />
     <App initialPosts={initialPosts} />
   </StrictMode>,
 )
